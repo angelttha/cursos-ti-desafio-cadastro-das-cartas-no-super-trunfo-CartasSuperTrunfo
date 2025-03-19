b675f35c40a7fd6949b307ebed28fffac0af6c55
angelttha <byclown004@gmail.com>
2025-03-19 01:02:38 +0000

    Meu trabalho CartasSuperTrunfo nível aventureiro

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,41 +1,66 @@
 #include <stdio.h>
 
 int main() {
-  printf("SUPER TRUNFO\n"); 
-  printf("Carta 1\n");
-     char estado = 'A';
-     char codigodacarta [50] = "S01";
-     char cidade [50] = "São Paulo";
-     int populacao = 11451245;
-     float areakm = 1528.5;
-     float pib = 60340000000;
-     int numerosdepontosturisticos = 60;
+  printf("SUPER TRUNFO\n");
+  printf("PAÍSES\n");
+  printf("______\n");
 
-     printf("Estado: %c \n", estado);
-     printf("Código da carta: %s \n", codigodacarta);
-     printf("Cidade: %s \n", cidade);
-     printf("População: %d \n", populacao);
-     printf("Área Km2: %f \n", areakm);
-     printf("PIB: %e \n", pib);
-     printf("Números de pontos túristicos: %d \n", numerosdepontosturisticos);
+  //Primeira carta
+   char pais [50] = "Brasil";
+   char codigo_da_carta [50] = "A01";
+   char nome_do_estado[50] = "São Paulo";
+   int populacao = 114500000;
+   float area_em_km = 1521;
+   float pib  = 271975123100;
+   int numero_de_pontos_turisticos = 60;
+   float densidade_populacional = 752826;
+   float pib_per_capita = 7270;
 
-     printf("Carta 2\n");
-     char estado2 = 'B';
-     char codigodacarta2 [50] = "R02";
-     char cidade2 [50] = "Rio de Janeiro";
-     int populacao2 = 6729894;
-     float areakm2 = 12553;
-     float pib2 = 1150000000000;
-     int numerosdepontosturisticos2 = 80;
+     int media;
+       media = densidade_populacional / pib_per_capita;
 
-     printf("Estado: %c \n", estado2);
-     printf("Código da carta: %s \n", codigodacarta2);
-     printf("Cidade: %s \n", cidade2);
-     printf("População: %d \n", populacao2);
-     printf("Área Km2: %f \n", areakm2);
-     printf("PIB: %e \n", pib2);
-     printf("Números de pontos túristicos: %d \n", numerosdepontosturisticos2);
+   printf("País: %s\n", pais);
+   printf("Código da Carta: %s\n", codigo_da_carta);
+   printf("Nome do Estado: %s\n", nome_do_estado);
+   printf("População: %d\n", populacao);
+   printf("Área em Km2: %.2f\n",area_em_km);
+   printf("PIB: %.2f\n", pib);
+   printf("Números de Pontos Túristicos: %d\n", numero_de_pontos_turisticos);
+     //Média
+       printf("Densidade Populacional: %f\n", densidade_populacional);
+       printf("PIB Per Capita: %f\n", pib_per_capita);
+         printf("Densidade Populacional e PIB Per Capita: %d\n", media);
 
+   printf("_______\n");   
+   
+     //Segunda carta
+      char pais2 [50] = "Estados Unidos";
+      char codigo_da_carta2 [50] = "B05";
+      char nome_do_estado2[50] = "Nova York";
+      int populacao2 = 825800000;
+      float area_em_km2 = 7838;
+      float pib2  = 20000000000000;
+      int numero_de_pontos_turisticos2 = 75;
+      float densidade_populacional2 = 752826;
+      float pib_per_capita2 = 7270;
+   
+        int media2;
+          media2 = densidade_populacional2 / pib_per_capita2;
+   
+      printf("País: %s\n", pais2);
+      printf("Código da Carta: %s\n", codigo_da_carta2);
+      printf("Nome do Estado: %s\n", nome_do_estado2);
+      printf("População: %d\n", populacao2);
+      printf("Área em Km2: %.2f\n",area_em_km2);
+      printf("PIB: %.2f\n", pib2);
+      printf("Números de Pontos Túristicos: %d\n", numero_de_pontos_turisticos2);
+        //Média
+          printf("Densidade Populacional: %f\n", densidade_populacional2);
+          printf("PIB Per Capita: %f\n", pib_per_capita2);
+            printf("Densidade Populacional e PIB Per Capita: %d\n", media2);
+   
+      printf("_______\n"); 
+    
 
     return 0;
 }
